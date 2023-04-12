22667189d6f0a9fc01145c9e345b74473a112cf4
Sara Raley <sara.m.raley@gmail.com>
2023-04-12 11:11:24 -0600

    adds update form to modal

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -5,6 +5,15 @@ export function ProductsShow(props) {
       <p>{props.product.name}</p>
       <p>${props.product.price}</p>
       <p>{props.product.description}</p>
+      <form>
+        <div>
+          Name: <input defaultValue={props.product.name} name="name" type="text" />
+          Price: $ <input defaultValue={props.product.price} name="price" type="text" />
+          Image: <input defaultValue={props.product.image} name="image" type="text" />
+          Description: <input defaultValue={props.product.description} name="description" type="text" />
+        </div>
+        <button type="submit">Update product</button>
+      </form>
     </div>
   );
 }
